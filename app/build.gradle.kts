f86ecd5ec01f95b7e2fb2dd9d31ffc62c543f5f0
Damien GAILLARD <damien.gaillard86@gmail.com>
2025-10-01 16:19:10 +0200

    release: 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "fr.zunkit.motorcyclemanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.1"
+        versionCode = 12
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
