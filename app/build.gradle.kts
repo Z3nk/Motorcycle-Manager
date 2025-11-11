30e4a9c888ab04a4ed2290b0840e98d4626d263f
Damien GAILLARD <damien.gaillard86@gmail.com>
2025-11-11 20:44:28 +0100

    release: 1.7.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "fr.zunkit.motorcyclemanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 172
-        versionName = "1.7.2"
+        versionCode = 173
+        versionName = "1.7.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
